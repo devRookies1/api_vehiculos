1fe912fdca7433d7fa2e36034d21b0e5f9e99b0c
Vanessa <vane789vane@hotmail.com>
2021-10-09 09:33:56 -0500

    agregar controladores

---
+++ controllers/vehiculos/controller.js
@@ -0,0 +1,44 @@
+import { ObjectId } from 'mongodb';
+import { getDB } from '../../db/db.js';
+
+const queryAllVehicles = async (callback) => {
+  const baseDeDatos = getDB();
+  await baseDeDatos.collection('vehiculos').find({}).limit(50).toArray(callback);
+};
+
+const crearVehiculo = async (datosVehiculo, callback) => {
+  if (
+    Object.keys(datosVehiculo).includes('Nombre') &&
+    Object.keys(datosVehiculo).includes('ValorUnitario') &&
+    Object.keys(datosVehiculo).includes('Estado')
+  ) {
+    const baseDeDatos = getDB();
+    await baseDeDatos.collection('vehiculos').insertOne(datosVehiculo, callback);
+  } else {
+    return 'error';
+  }
+};
+
+const consultarVehiculo = async (id, callback) => {
+  const baseDeDatos = getDB();
+  await baseDeDatos.collection('vehiculos').findOne({ _id: new ObjectId(id) }, callback);
+};
+
+const editarVehiculo = async (id, edicion, callback) => {
+  const filtroVehiculo = { _id: new ObjectId(id) };
+  const operacion = {
+    $set: edicion,
+  };
+  const baseDeDatos = getDB();
+  await baseDeDatos
+    .collection('vehiculos')
+    .findOneAndUpdate(filtroVehiculo, operacion, { upsert: true, returnOriginal: true }, callback);
+};
+
+const eliminarVehiculo = async (id, callback) => {
+  const filtroVehiculo = { _id: new ObjectId(id) };
+  const baseDeDatos = getDB();
+  await baseDeDatos.collection('vehiculos').deleteOne(filtroVehiculo, callback);
+};
+
+export { queryAllVehicles, crearVehiculo, consultarVehiculo, editarVehiculo, eliminarVehiculo };
