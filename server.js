3a7cb0cc2d2afb1128e8d1cff1bf26d48706271f
Vanessa <vane789vane@hotmail.com>
2021-10-17 23:07:37 -0500

    agregar auth y configurar server

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import { conectarBD } from './db/db.js';
 import rutasVehiculo from './views/vehiculos/rutas.js';
 import rutasUsuario from './views/usuarios/rutas.js';
 import rutasVenta from './views/ventas/rutas.js';
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
 
 dotenv.config({ path: './.env' });
 
@@ -15,6 +17,21 @@ app.use(Cors());
 app.use(rutasVehiculo);
 app.use(rutasUsuario);
 app.use(rutasVenta);
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://devrookies-vehiculos.us.auth0.com/.well-known/jwks.json',
+  }),
+  audience: 'api-autenticacion-vehiculos',
+  issuer: 'https://devrookies-vehiculos.us.auth0.com/',
+  algorithms: ['RS256'],
+});
+
+app.use(jwtCheck);
+
 
 const main = () => {
   return app.listen(process.env.PORT, () => {
