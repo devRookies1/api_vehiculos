16d1d2030c31fd73ef14ac4528101366e7ccbb36
Vanessa <vane789vane@hotmail.com>
2021-10-25 09:43:24 -0500

    cambios controlador usuarios

--- controllers/usuarios/controller.js
+++ controllers/usuarios/controller.js
@@ -5,30 +5,34 @@ import jwt_decode from 'jwt-decode';
 const queryAllUsers = async (callback) => {
   const baseDeDatos = getDB();
   console.log('query');
-  await baseDeDatos.collection('usuarios').find({}).limit(50).toArray(callback);
+  await baseDeDatos.collection('usuario').find({}).limit(50).toArray(callback);
 };
 
 const crearUsuario = async (datosUsuario, callback) => {
   const baseDeDatos = getDB();
-  await baseDeDatos.collection('usuarios').insertOne(datosUsuario, callback);
+  await baseDeDatos.collection('usuario').insertOne(datosUsuario, callback);
 };
 
 const consultarUsuario = async (id, callback) => {
   const baseDeDatos = getDB();
-  await baseDeDatos.collection('usuarios').findOne({ _id: new ObjectId(id) }, callback);
+  await baseDeDatos.collection('usuario').findOne({ _id: new ObjectId(id) }, callback);
 };
 
 const consultarOCrearUsuario = async (req, callback) => {
-  console.log('Estoy llegando a crear usuario');
+
   const token = req.headers.authorization.split('Bearer ')[1];
   const user = jwt_decode(token)['http://localhost/userData'];
   console.log(user);
+
+ 
   const baseDeDatos = getDB();
   await baseDeDatos.collection('usuario').findOne({ email: user.email }, async (err, response) => {
     console.log('response consulta bd', response);
     if (response) {
+     
       callback(err, response);
     } else {
+      
       user.auth0ID = user._id;
       delete user._id;
       user.rol = 'sin rol';
@@ -37,6 +41,7 @@ const consultarOCrearUsuario = async (req, callback) => {
     }
   });
 };
+
 const editarUsuario = async (id, edicion, callback) => {
   const filtroUsuario = { _id: new ObjectId(id) };
   const operacion = {
@@ -44,13 +49,21 @@ const editarUsuario = async (id, edicion, callback) => {
   };
   const baseDeDatos = getDB();
   await baseDeDatos
-    .collection('usuarios')
+    .collection('usuario')
     .findOneAndUpdate(filtroUsuario, operacion, { upsert: true, returnOriginal: true }, callback);
 };
+
 const eliminarUsuario = async (id, callback) => {
   const filtroUsuario = { _id: new ObjectId(id) };
   const baseDeDatos = getDB();
-  await baseDeDatos.collection('usuarios').deleteOne(filtroUsuario, callback);
+  await baseDeDatos.collection('usuario').deleteOne(filtroUsuario, callback);
 };
 
-export { queryAllUsers, crearUsuario, consultarUsuario, editarUsuario, eliminarUsuario, consultarOCrearUsuario};
+export {
+  queryAllUsers,
+  crearUsuario,
+  consultarUsuario,
+  editarUsuario,
+  eliminarUsuario,
+  consultarOCrearUsuario,
+};
